9b14d8a48d2cd55f06fe78d11fa0720978a8822f
Khymari Sandy <khymari.sandy@dawsoncollege.qc.ca>
2026-01-25 20:10:19 -0500

    Add more to small image display

--- portfolio/src/components/AccentDisplayWithImage.jsx
+++ portfolio/src/components/AccentDisplayWithImage.jsx
@@ -1,7 +1,10 @@
-function smallImageDisplay() {
-  
-  
+function smallImageDisplay({imagepath, text, title}) {
   return (
-  
+    <section>
+      <img src={imagepath} alt={title} />
+      <div className="border">
+        <p>{text}</p>
+      </div>
+    </section>
   );
 }
