65d7bf2b57ef37170cbad00bd023e3c791a088e5
Khy_Sandy <khymari.sandy@dawsoncollege.qc.ca>
2026-01-27 20:31:49 -0500

    fix styling

--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -8,10 +8,14 @@ export function slideshowDisplay({items}) {
       <Fade>
         {items.map((item, index) => (
           <div key={index}>
-            <img src={item.image}/>
+            <div className="flex justify-center">
+              <img src={item.image}/>
+            </div>
             <h3 className="text-l font-semibold text-[#e0b3e8] text-center">{item.title}</h3>
-            <p className="text-white">{item.description}</p>
-            <a href={item.link} className="text-[#e0b3e8] text-center w-full block">Project Link</a>
+            <p className="text-white text-center">{item.description}</p>
+            <div className="flex justify-center">
+              <a href={item.link} className="text-[#e0b3e8] block underline">Project Link</a>
+            </div>
           </div>
         ))}
       </Fade>
