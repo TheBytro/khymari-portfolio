57c6297d549ff9d5bb8ff1c80b58614fec271517
Khy_Sandy <khymari.sandy@dawsoncollege.qc.ca>
2026-01-27 20:31:38 -0500

    Add more projects

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,4 +1,4 @@
-import {smallImageDisplay as SmallDisplay} from "./components/AccentDisplayWithImage.jsx";
+import {smallImageDisplay as SmallDisplay, experienceImageDisplay as ExperienceDisplay} from "./components/AccentDisplayWithImage.jsx";
 import NavBar from './components/Navigation.jsx'
 import {aboutme as About} from "./components/Description.jsx";
 import './App.css'
@@ -8,6 +8,8 @@ import codingImage from './assets/programming.png';
 import toolsImage from './assets/tools.png';
 import techImage from './assets/computer.png';
 import darkThemeImage from './assets/darkTheme.png';
+import readingImage from './assets/readingapp.jpg'
+import irentImage from './assets/irent.jpg';
 
 const projectItems = [
   {
@@ -18,9 +20,15 @@ const projectItems = [
   },
   {
     image: darkThemeImage,
-    title: 'Penguin Technologies 2',
-    description: 'An Avalonia-based project designed for an IT help desk company that incorporates Google Maps API for remote assistance.',
-    link: 'https://github.com/TheBytro/penguin-technologies',
+    title: 'GameShop',
+    description: 'A project utilizing Java Swing to display a clean GUI for purchasing games from a generic game store',
+    link: 'https://gitlab.com/byronyang1/gameshop_programming_fall_2024',
+  },
+  {
+    image: readingImage,
+    title: 'Happy Reading App',
+    description: 'An android mobile app that allows for online downloading and parsing of books to read offline',
+    link: 'https://gitlab.com/byronyang1/gameshop_programming_fall_2024',
   }
 
 ]
@@ -32,7 +40,7 @@ function App() {
       <NavBar />
       <section className="translate-y-30 md:translate-y-20 px-4 flex flex-col">
         <About />
-        <section className="mt-5 scroll-mt-28" id="skills">
+        <section className="mt-5 scroll-mt-28 md:scroll-mt-20" id="skills">
           <h2 className="text-xl font-semibold text-[#e0b3e8] text-center">My Skills:</h2>
           <section className="flex flex-row place-content-evenly flex-wrap">
             <SmallDisplay title="Back-End Development"
@@ -46,8 +54,23 @@ function App() {
                           image={techImage}/>
           </section>
         </section>
-        <section className="mt-5 scroll-mt-28 flex flex-col" id="projects">
+        <section className="mt-5 scroll-mt-28 md:scroll-mt-20 flex flex-col" id="projects">
           <Projects items={projectItems} />
+        </section>
+        <section className="mt-5 scroll-mt-28 md:scroll-mt-20" id="experiences">
+          <h2 className="text-xl font-semibold text-[#e0b3e8] text-center">My Experiences:</h2>
+          <section className="md:flex md:flex-row md:place-content-evenly gap-4">
+            <ExperienceDisplay title="IT Project Manager"
+                               company="InterRent REIT"
+                               list={["Software development integration projects", "Integrated Smart Building Thermostat with Data Warehouse software with custom python API", "Integrated Cloud-based HCM and Expense system", "Consolidation of financial information for SaaS solutions for executive presentation", "Helpdesk tasks"]}
+                               timespan="06/2024 - 08/2024"
+                               image={irentImage}/>
+            <ExperienceDisplay title="IT Help Desk Student Co-op"
+                               company="InterRent REIT"
+                               timespan="07/2025 - 08/2025"
+                               list={["3rd most valuable tech employee", "Troubleshoot and reconfigure laptops for windows Autopilot", "Upgraded Company workstations", "Performed offsite device installations and repairs"]}
+                               image={irentImage}/>
+          </section>
         </section>
       </section>
     </>
